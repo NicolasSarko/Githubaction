072e3a547efd406c446b671de41effa6efa72d32
NicolasSarko <48804214+NicolasSarko@users.noreply.github.com>
2021-05-06 17:48:41 +0200

    Update test.sh

--- action-a/test.sh
+++ action-a/test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bats
 
 @test "Should pass" {
- result="$(INPUT_MY_NAME='Mona' ./entrypoint.sh)"
+ result="$(INPUT_MY_NAME='Mona' ./action-a/entrypoint.sh)"
  [ $? -eq 0 ]
 }
 
 @test "This test is a bug" {
- result="$(INPUT_MY_NAME='aMona' ./entrypoint.sh)"
+ result="$(INPUT_MY_NAME='aMona' ./action-a/entrypoint.sh)"
  [ $? -eq 1 ]
 }
